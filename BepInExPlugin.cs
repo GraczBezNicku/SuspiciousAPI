1e344908b4b962afe68fd926c51953df04794051
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2024-03-27 16:15:41 +0100

    Project setup

--- BepInExPlugin.cs
+++ BepInExPlugin.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SuspiciousAPI
+namespace SuspiciousAPI;
+
+[BepInPlugin]
+public class BepInExPlugin
 {
-    public class BepInExPlugin
-    {
-    }
+
 }
