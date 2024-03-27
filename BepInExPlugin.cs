2aec35411ac619020c91835e1e9873d0b017b532
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2024-03-27 21:33:53 +0100

    Basic init stuff

--- BepInExPlugin.cs
+++ BepInExPlugin.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,21 @@ using System.Threading.Tasks;
 
 namespace SuspiciousAPI;
 
-[BepInPlugin]
-public class BepInExPlugin
+[BepInPlugin("gbn.suspiciousapi", "Suspicious Modding API", "1.0.0.0")]
+public class BepInExPlugin : BasePlugin
 {
+    public Harmony _harmony;
+    public static ManualLogSource log;
 
+    public override void Load()
+    {
+        log = Log;
+
+        _harmony = new Harmony($"GBN-SUSPICIOUSAPI");
+        _harmony.PatchAll();
+
+        // INIT
+
+        Log.LogMessage($"Suspicious API has been initialized!");
+    }
 }
