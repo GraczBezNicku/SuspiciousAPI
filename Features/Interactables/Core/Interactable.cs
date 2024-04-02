3609298c41665386bedb9d95f2515bd00a267bb4
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2024-04-02 20:51:24 +0200

    Base for interactables API
    Need to wait for response from BepInEx discord

--- Features/Interactables/Core/Interactable.cs
+++ Features/Interactables/Core/Interactable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SuspiciousAPI.Features.Interactables.Core;
 
@@ -11,5 +12,91 @@ namespace SuspiciousAPI.Features.Interactables.Core;
 /// </summary>
 public class Interactable
 {
+    public static Dictionary<GameObject, Interactable> GameObjectToInteractable = new Dictionary<GameObject, Interactable>();
 
+    private IUsable _usable;
+    public IUsableOverrides usableOverrides;
+
+    public Interactable(IUsable usable)
+    {
+        _usable = usable;
+        usableOverrides = new IUsableOverrides(usable);
+    }
+
+    public IUsable Usable
+    {
+        get => _usable;
+        set
+        {
+            _usable = value;
+            usableOverrides = new IUsableOverrides(value);
+        }
+    }
+
+    public float MaxDistance
+    {
+        get => usableOverrides.UsableDistance;
+        set => usableOverrides.UsableDistance = value;
+    }
+
+    public static Interactable Get(GameObject gameObject)
+    {
+        if (GameObjectToInteractable.ContainsKey(gameObject))
+            return GameObjectToInteractable[gameObject];
+
+        GameObject foundObject = null;
+        foreach (Component comp in gameObject.GetComponents<Component>())
+        {
+            if (comp is not IUsable usable)
+                continue;
+        }
+    }
+}
+
+public struct IUsableOverrides
+{
+    private float _usableDistance;
+    private ImageNames _useIcon;
+    private float _percentCool;
+
+    public float UsableDistance
+    {
+        get => _usableDistance;
+        set
+        {
+            _usableDistance = value;
+            isDistanceOverriden = true;
+        }
+    }
+
+    public ImageNames UseIcon
+    {
+        get => _useIcon;
+        set
+        {
+            _useIcon = value;
+            isIconOverriden = true;
+        }
+    }
+
+    public float PercentCool
+    {
+        get => _percentCool;
+        set
+        {
+            _percentCool = value;
+            isPercentOverriden = true;
+        }
+    }
+
+    public bool isDistanceOverriden = false;
+    public bool isIconOverriden = false;
+    public bool isPercentOverriden = false;
+
+    public IUsableOverrides(IUsable usable)
+    {
+        _usableDistance = usable.UsableDistance;
+        _useIcon = usable.UseIcon;
+        _percentCool = usable.PercentCool;
+    }
 }
---
+++ Features/Interactables/Patches/UsableGettersPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using SuspiciousAPI.Features.Interactables.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuspiciousAPI.Features.Interactables.Patches;
+
+[HarmonyPatch(typeof(IUsable))]
+public static class UsableGettersPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(IUsable), nameof(IUsable.UsableDistance), MethodType.Getter)]
+    public static bool UsableDistancePrefix(ref float __result)
+    {
+        Interactable interactable = 
+    }
+}
